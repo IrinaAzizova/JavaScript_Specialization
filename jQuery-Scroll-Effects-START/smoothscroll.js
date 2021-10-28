c1504d3047c5fbcaa4cfd73e36ee11fbfe964b0b
Irina Azizova <i_r_i_n_a_nikitina@mail.ru>
2021-10-28 18:05:14 +0300

    scroll & page location tracking with jQuery

--- jQuery-Scroll-Effects-START/smoothscroll.js
+++ jQuery-Scroll-Effects-START/smoothscroll.js
@@ -1,2 +1,62 @@
-// Smoothscroll Script...
+$('nav ul li a').click( function(){
+    const thisSection = $(this).attr('href');
+    const thisLink = $(this);
 
+    $('html, body').stop().animate({
+        scrollTop: $(thisSection).offset().top - 200
+    }, 500, 'easeOutCirc');
+    return false;
+});
+
+$(window).on('load', function(){
+    const allLinks = $('nav ul li a');
+    const posts = $('section');
+    let pagetop;
+    let counter = 0;
+    let prevCounter = 0;
+    let doneResizing;
+
+    let posTops = [];
+
+    posts.each( function() {
+        posTops.push( Math.floor( $(this).offset().top) );
+    });
+
+    $(window).scroll(function(){
+        pagetop = $(window).scrollTop() + 210;
+
+        if (pagetop > posTops[counter+1]) {
+            counter++;
+        }
+        else if (counter > 0 && pagetop < posTops[counter]) {
+            counter--;
+        }
+
+        if (counter != prevCounter){
+            $(allLinks).removeAttr('class');
+            $('nav ul li a').eq(counter).addClass('selected');
+            prevCounter = counter;
+        }        
+    });
+
+    $(window).on('resize', function(){
+        clearTimeout(doneResizing);
+        doneResizing = setTimeout(function(){
+            posTops = [];
+            posts.each( function() {
+                posTops.push( Math.floor( $(this).offset().top) );
+            });
+
+            const pagePosition = $(window).scrollTop()+210;
+            counter = 0;
+
+            for ( let i=0; i<posTops.length; i++) {
+                if (pagePosition > posTops[i]) {counter++;}
+            }
+            counter--;
+
+            $(allLinks).removeAttr('class');
+            $('nav ul li  a').eq(counter).addClass('selected');
+        }, 500);
+    });
+});
