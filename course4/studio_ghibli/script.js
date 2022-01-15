f71bb74f52be33882aa1ac3d99397002033831a7
Irina Azizova <i_r_i_n_a_nikitina@mail.ru>
2022-01-15 22:21:13 +0300

    studio Ghibli part 2. end of course

--- course4/studio_ghibli/script.js
+++ course4/studio_ghibli/script.js
@@ -1,18 +1,30 @@
 const mainElement = document.querySelector('main');
+const navLinks = document.querySelectorAll('#mainnav ul li a');
 
 let filmData;
+let dataSet = 'films';
+let url = 'https://ghibliapi.herokuapp.com/films';
 
-async function getFilms() {
-    const filmPromise = await fetch('https://ghibliapi.herokuapp.com/films');
-    const films = await filmPromise.json();
 
-    setSort(films);
-    addCard(films);
-    filmData = films;
+async function getData(url) {
+    const dataPromise = await fetch(url);
+    const data = await dataPromise.json();
 
-    document.querySelector('#sortorder').removeAttribute('disabled');
+    if (dataSet === 'films') {
+        mainElement.innerHTML = '';
+        setSort(data);
+        addCard(data);
+        filmData = data;
+        document.getElementById('sortorder').removeAttribute('disabled');
+        document.querySelector('nav form').style.visibility = "visible";
+        }
+    else {
+        mainElement.innerHTML = '';
+        addCard(data);
+        document.querySelector('nav form').style.visibility = "hidden";
+    } 
 }
-getFilms();
+getData(url);
 
 function addCard(array) {
     array.forEach( EachItem => {
@@ -20,36 +32,120 @@ function addCard(array) {
     })
 }
 
-function createCard(data) {
+async function createCard(data) {
     const card = document.createElement('article');
-
-    const movieTitle = document.createElement('h2');
-    const movieTitleTxt = document.createTextNode(data.title);
-    movieTitle.appendChild(movieTitleTxt);
-
-    const director = document.createElement('p');
-    const directorTxt = document.createTextNode(`Director: ${data.director}`);
-    director.appendChild(directorTxt);
-
-    const year = document.createElement('p');
-    const yearTxt = document.createTextNode(`Released: ${data.release_date}`);
-    year.appendChild(yearTxt);
-
-    const description = document.createElement('p');
-    const descriptionTxt = document.createTextNode(`Description: ${data.description}`);
-    description.appendChild(descriptionTxt);
-
-    const score = document.createElement('p');
-    const scoreTxt = document.createTextNode(`Rotten Tomatoes score: ${data.rt_score}`);
-    score.appendChild(scoreTxt);
-
-    card.appendChild(movieTitle);
-    card.appendChild(director);
-    card.appendChild(year);
-    card.appendChild(description);
-    card.appendChild(score);
-
+    switch (dataSet) {
+        case 'films': card.innerHTML = filmCardContents(data); break;
+        case 'people': card.innerHTML = await peopleCardContents(data); break;
+        case 'locations': card.innerHTML = await locationCardContents(data); break;
+        case 'species': card.innerHTML = await speciesCardContent(data); break;
+        case 'vehicles': card.innerHTML = await vehiclesCardContent(data); break;
+    }       
     mainElement.appendChild(card);
+}
+
+function filmCardContents(data) {
+    let html = `<h2>${data.title}</h2>`;
+    html += `<p><strong>Director:</strong> ${data.director}</p>`;
+    html += `<p><strong>Released:</strong> ${data.release_date}</p>`;
+    html += `<p>${data.description}</p>`;
+    html += `<p><strong>Rotten Tomatoes Score:</strong> ${data.rt_score}</p>`;
+    return html;
+}
+
+async function peopleCardContents(data) {
+
+    const thefilms = data.films;
+    let filmtitles = [];
+    for (eachFilm of thefilms) {
+        const filmTitle = await indivItem(eachFilm, 'title');
+        filmtitles.push(filmTitle);
+    }
+
+    const species = await indivItem(data.species, 'name');
+    let html = `<h2>${data.name}</h2>`;
+    html += `<p><strong>Details:</strong> gender ${data.gender}, age ${data.age}, eye color
+   ${data.eye_color}, hair color ${data.hair_color}</p>`;
+    html += `<p><strong>Species:</strong> ${species}</p>`;
+    html += `<p><strong>Films:</strong> ${filmtitles.join(', ')}</p>`;
+    return html;
+}
+
+async function locationCardContents(data) {
+    const regex = 'https?:\/\/';
+    const theResidents = data.residents;
+    let residentNames = [];
+
+    for (eachResident of theResidents) {
+        if(eachResident.match(regex)){
+            const resName = await indivItem(eachResident, 'name');
+            residentNames.push(resName);
+        }
+        else {
+            residentNames[0]='no data available';
+        }
+    }
+
+    const thefilms = data.films;
+    let filmtitles = [];
+    for (eachFilm of thefilms) {
+        const filmTitle = await indivItem(eachFilm, 'title');
+        filmtitles.push(filmTitle);
+    }
+
+    let html = `<h2>${data.name}</h2>`;
+    html += `<p><strong>Details:</strong> climate ${data.climate}, terrain ${data.terrain}, surface water
+   ${data.surface_water}%</p>`;
+    html += `<p><strong>Residents:</strong> ${residentNames.join(', ')}</p>`;
+    html += `<p><strong>Films:</strong> ${filmtitles.join(', ')}</p>`;
+    return html;
+}
+
+async function speciesCardContent(data) {
+    const people = data.people;
+    let peopleNames = [];
+    for (eachMan of people) {
+        const man = await indivItem(eachMan, 'name')
+        peopleNames.push(man);
+    }
+    const thefilms = data.films;
+    let filmtitles = [];
+    for (eachFilm of thefilms) {
+        const filmTitle = await indivItem(eachFilm, 'title');
+        filmtitles.push(filmTitle);
+    }
+
+    let html = `<h2>${data.name}</h2>`;
+    html += `<p><strong>Classification:</strong> ${data.classification}</p>`;
+    html += `<p><strong>Eye colors:</strong> ${data.eye_colors}</p>`;
+    html += `<p><strong>Eye colors:</strong> ${data.hair_colors}</p>`;
+    html += `<p><strong>People:</strong> ${peopleNames.join(', ')}</p>`;
+    html += `<p><strong>Films:</strong> ${filmtitles.join(', ')}</p>`;
+    
+    return html;
+}
+
+async function vehiclesCardContent(data) {    
+    let html = `<h2>${data.name}</h2>`;
+    html += `<p><strong>Description:</strong> ${data.description}</p>`;
+    html += `<p><strong>Vehicle Class:</strong> ${data.vehicle_class}</p>`;
+    html += `<p><strong>Length:</strong> ${data.length} feet</p>`;
+    html += `<p><strong>Pilot:</strong> ${await indivItem(data.pilot, 'name')}</p>`;
+    html += `<p><strong>Film:</strong> ${await indivItem(data.films, 'title')}</p>`;
+    return html;
+}
+
+async function indivItem(url, item) {
+    var theItem;
+    try{
+        const itemPromise = await fetch(url);
+        const data = await itemPromise.json();
+        theItem = data[item];
+    } catch(err){
+        theItem = "no data available";
+    } finally{
+        return theItem;
+    }
 }
 
 
@@ -57,6 +153,17 @@ document.querySelector('#sortorder').addEventListener('change', () => {
     mainElement.innerHTML = '';
     setSort(filmData);
     addCard(filmData);
+});
+
+navLinks.forEach(function (eachLink) {
+    eachLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    const thisLink = event.target.getAttribute('href').substring(1);
+    //alert(thisLink);
+    dataSet = thisLink;
+    url = `https://ghibliapi.herokuapp.com/${thisLink}`;
+    getData(url);
+    });
 });
 
 function setSort(array) {
